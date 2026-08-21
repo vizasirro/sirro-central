967acc6ca9231ac8ea4fce201ef5ebc91c64c08e
vizasirro <victorzepedaprint@gmail.com>
2026-08-21 04:56:52 -0600

    Reparar SIRRO y agregar recuperación y ayuda
    Revisión de punta a punta aplicada conforme a la regla de oro.

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='sirro-v018';
-const ASSETS=['./','./index.html','./styles.css','./facilities-data.js','./app.js','./manifest.webmanifest','./icon-192.png','./icon-512.png','./logo-region-olancho.png'];
+const CACHE='sirro-v020';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./logo-region-olancho.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const clone=r.clone();caches.open(CACHE).then(c=>c.put(e.request,clone));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
